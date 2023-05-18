e098acf72e89f56f650b8c3c0c73db649b60b644
Shailesh Panthee <shaileshpanthee@gmail.com>
2023-05-18 20:11:33 +0545

    First working version, first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { MeckelsDiverticulum } from "./Subjects/Surgery/MeckelsDiverticulum";
+import "./App.css";
+
+function QuestionCard() {
+  const [eachQues, setEachQues] = React.useState(MeckelsDiverticulum);
+  const questionAndAnswer = MeckelsDiverticulum.map((obj) => {
+    function toggleShow() {
+      setEachQues((prev) => {
+        return [...prev, (obj.show = !obj.show)];
+      });
+      console.log("Clicked");
+    }
+    console.log(obj.answer);
+    return (
+      <div className="both-ques-ans-card">
+        <div className="questionAndSubmit">
+          <div className="question">{obj.question}</div>
+          <button className="show-button" onClick={toggleShow}>
+            Show
+          </button>
+        </div>
+        <div className="answer">{obj.show && obj.answer}</div>
+      </div>
+    );
+  });
+
+  return <div>{questionAndAnswer}</div>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>Meckel's Diverticulum</div>
+      <hr />
+      <div>
+        <QuestionCard />
+      </div>
+    </>
   );
 }
 
---
+++ src/Subjects/Surgery/MeckelsDiverticulum.js
@@ -0,0 +1,30 @@
+const MeckelsDiverticulum = [
+  {
+    question: "Is it true/false diverticulum? Why?",
+    answer: "True Diverticulum. It contains all layers of bowel wall.",
+    show: false,
+  },
+  {
+    question: "It is located along ...... border of ileum",
+    answer: "Anti-mesenteric",
+    show: false,
+  },
+  {
+    question: "Incidence in Male vs Female?",
+    answer: "Equal",
+    show: false,
+  },
+  {
+    question: "Most common heterotropic tissue found?",
+    answer: "Gastric Mucosa (50%)",
+    show: false,
+  },
+  {
+    question: "Clinical Features?",
+    answer:
+      "1. Most common clinical feature: GI bleeding \n2. Hemorrhage is manifested as Painless bright red blood from rectum. \n3. Source of bleeding is Ulcer in ileum adjacent to Meckel’s diverticulum that contains gastric mucosa (due to HCl exposure) \n4. Intestinal obstruction due to volvulus, intussusception",
+    show: true,
+  },
+];
+
+export { MeckelsDiverticulum };
